ff2d1cef643157962d58cd22312ddf59e96d8e16
Art1mod <artmod2112@gmail.com>
2026-02-28 14:57:50 +0100

    feat: initial commit

---
+++ Source/silent_partner/silent_partner.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "silent_partner.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, silent_partner, "silent_partner" );
